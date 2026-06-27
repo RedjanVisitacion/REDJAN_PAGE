8de2ef28538e5de4d90168fb3a0a114e2e4fe7dd
RedjanVisitacion <visitacionredjan7@gmail.com>
2026-06-27 13:43:08 +0800

    URL Hash

---
+++ php/route_config.php
@@ -0,0 +1,44 @@
+<?php
+return [
+    'home' => 'index.html',
+    'routes' => [
+        '6fa3f52ec-a830-83ec-aeb0-a1b2fb7fd22d' => [
+            'name' => 'login',
+            'file' => 'html/Login.html',
+            'title' => 'Login',
+        ],
+        'b8d4f10a-19e6-4f43-965a-26f7b0b7f4c9' => [
+            'name' => 'certificate',
+            'file' => 'html/Certificate.html',
+            'title' => 'Certificate',
+        ],
+        '8c9e7024-bb28-4aa5-9f41-1976d8a55d50' => [
+            'name' => 'verify',
+            'file' => 'html/verify.html',
+            'title' => 'Verify Email',
+        ],
+        '3d7bd58f-6c3d-4478-8325-a977d9d679f4' => [
+            'name' => 'reset',
+            'file' => 'html/reset.html',
+            'title' => 'Reset Password',
+        ],
+        'a73965fb-d7e7-43d8-8e6f-420ef2080ed8' => [
+            'name' => 'game',
+            'file' => 'html/MyGame.html',
+            'title' => 'Game',
+        ],
+        'dce2fb10-7f6d-4f48-9fd4-77f1d2aa75c4' => [
+            'name' => 'ai',
+            'file' => 'html/RPSV_AI.html',
+            'title' => 'AI',
+        ],
+    ],
+    'aliases' => [
+        'login' => '6fa3f52ec-a830-83ec-aeb0-a1b2fb7fd22d',
+        'certificate' => 'b8d4f10a-19e6-4f43-965a-26f7b0b7f4c9',
+        'verify' => '8c9e7024-bb28-4aa5-9f41-1976d8a55d50',
+        'reset' => '3d7bd58f-6c3d-4478-8325-a977d9d679f4',
+        'game' => 'a73965fb-d7e7-43d8-8e6f-420ef2080ed8',
+        'ai' => 'dce2fb10-7f6d-4f48-9fd4-77f1d2aa75c4',
+    ],
+];
